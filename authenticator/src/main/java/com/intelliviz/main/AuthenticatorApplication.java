f81c91b197c99f56e1e7ab7c0dadf551fc02e618
emuhlestein <emuhlestein@yahoo.com>
2022-07-30 16:39:11 -0600

    Dockerfile and jenkinsfile changes

--- authenticator/src/main/java/com/intelliviz/main/AuthenticatorApplication.java
+++ authenticator/src/main/java/com/intelliviz/main/AuthenticatorApplication.java
@@ -3,6 +3,7 @@ package com.intelliviz.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
