2503a26aae435452537e161200ed7ed3c1947f0d
emuhlestein <emuhlestein@yahoo.com>
2022-07-30 11:39:13 -0600

    Initial commit for info-server

---
+++ src/test/java/com/intelliviz/infoserver/InfoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelliviz.infoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
