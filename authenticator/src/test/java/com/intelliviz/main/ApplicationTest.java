cd79c615140c2879ed4b8a8641b2c33cbe3749f4
emuhlestein <emuhlestein@yahoo.com>
2022-08-13 08:32:25 -0600

    Add unit test

---
+++ authenticator/src/test/java/com/intelliviz/main/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.intelliviz.main;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    @Test
+    public void test() {
+        System.out.println("Unit test was successful");
+    }
+}
